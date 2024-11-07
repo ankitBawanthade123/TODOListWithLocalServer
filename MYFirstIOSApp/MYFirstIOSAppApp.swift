ac20caa6212284c127509fde049f5255aa45c4c7
ankit2bawanthade <ankit2.bawanthade@rajasoftwarelabs.com>
2024-11-06 23:46:44 -0500

    Initial Commit

---
+++ MYFirstIOSApp/MYFirstIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MYFirstIOSAppApp.swift
+//  MYFirstIOSApp
+//
+//  Created by Ankit Bawanthade on 06/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MYFirstIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
